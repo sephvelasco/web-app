272402796cd64698d9776f20297884b372ff2b45
sephvelasco <marcusjoseph14@gmail.com>
2025-10-15 11:12:52 +0800

    Updated setup_win.bat

--- setup_win.bat
+++ setup_win.bat
@@ -1,17 +1,24 @@
+:: Windows Shell Script
 echo Setup of Web Application
+echo --------------------------
 
+:: Create Virtual Environment
 py -m venv venv
 call venv\Scripts\activate
 
+:: Upgrade pip Package Manager
 echo Upgrading pip...
 echo --------------------------
 py -m pip install --upgrade pip
 
+:: Install Dependencies
 echo Installing dependencies...
 echo --------------------------
 py -m pip install -r requirements.txt
 
+:: Start Flask
 echo Setup Complete!
+echo --------------------------
 echo Starting Flask...
 echo --------------------------
 py app.py
